9781011e951ce81152802818f882fa680ba0ba92
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-11-12 00:59:58 +0700

    it is finished

--- Assets/Resources/Scripts/Movements/CameraScript.cs
+++ Assets/Resources/Scripts/Movements/CameraScript.cs
@@ -18,19 +18,19 @@ public class CameraScript : MonoBehaviour
         // Gerakan translasi
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime);
+            transform.Translate(Vector3.forward * Time.deltaTime * 2);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime);
+            transform.Translate(Vector3.left * Time.deltaTime * 2);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * Time.deltaTime);
+            transform.Translate(Vector3.back * Time.deltaTime * 2);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime);
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
         }
 
         // Gerakan rotasi dengan mouse
